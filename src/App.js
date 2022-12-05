827c83632e89145c6c6ca0aecf0188c9598489b6
YOsF156 <yosistar@gmail.com>
2022-12-05 21:23:52 +0200

    clear some trash

--- src/App.js
+++ src/App.js
@@ -55,23 +55,3 @@ function App() {
 }
 
 export default App;
-
-
-// "eslintConfig": {
-//   "extends": [
-//     "react-app",
-//     "react-app/jest"
-//   ]
-// },
-// "browserslist": {
-//   "production": [
-//     ">0.2%",
-//     "not dead",
-//     "not op_mini all"
-//   ],
-//   "development": [
-//     "last 1 chrome version",
-//     "last 1 firefox version",
-//     "last 1 safari version"
-//   ]
-// }
